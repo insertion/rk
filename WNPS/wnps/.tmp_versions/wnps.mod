d656d7c1fb634bab2f88848a55416cb2eb07e53e
wang lei <398758695@qq.com>
2016-06-28 13:45:48 +0800

    rebuild

--- WNPS/wnps/.tmp_versions/wnps.mod
+++
@@ -1,2 +0,0 @@
-/home/user/rk/WNPS/wnps/wnps.ko
-/home/user/rk/WNPS/wnps/hook.o /home/user/rk/WNPS/wnps/kshell.o
--- WNPS/wnps/.wnps.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/user/rk/WNPS/wnps/wnps.o := ld -m elf_i386   -r -o /home/user/rk/WNPS/wnps/wnps.o /home/user/rk/WNPS/wnps/hook.o /home/user/rk/WNPS/wnps/kshell.o 
